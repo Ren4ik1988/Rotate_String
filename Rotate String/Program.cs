0fd4e30bb10c29b8e2f75b57579b72a250a60d55
Renat <ren.zigan@gmail.com>
2018-01-19 16:46:39 +0300

    Необходимо проверить еще раз цикл

--- Rotate String/Program.cs
+++ Rotate String/Program.cs
@@ -16,6 +16,7 @@ namespace Rotate_String
             for (int i = 0; i< doWork.CountOfTests; i++)
             {
                 doWork.InputTestLine();
+                doWork.Rotate();
                 for(int j = 0; j < doWork.ToRotate.Length; j++)
                 {
                     Console.Write(doWork.ToRotate[j]);
@@ -32,20 +33,58 @@ namespace Rotate_String
 
         private string[] input { get; set; }
 
-        public void HowManyTests() => CountOfTests = int.Parse(Console.ReadLine());
-        public void InputTestLine() => input = Console.ReadLine().Split(' ');
+        public void HowManyTests()
+        {
+            CountOfTests = int.Parse(Console.ReadLine());
+        }
+
+        public void InputTestLine()
+        {
+            input = Console.ReadLine().Split(' ');
+        }
 
         public void Rotate()
         {
             int rotateIndex = int.Parse(input[0]);
+            ToRotate = new char[input[1].Length];
             formCharArray();
-            
+            doRotate(rotateIndex);
+
         }
 
         private void formCharArray()
         {
             for (int i = 0; i < input[1].Length; i++)
                 ToRotate[i] = input[1][i];
+        }
+
+        private void doRotate(int rotateIndex)
+        {
+            bool needRevers = false;
+            if (rotateIndex < 0)
+            {
+                rotateIndex = Math.Abs(rotateIndex);
+                ToRotate.Reverse();
+                needRevers = true;
+            }
+
+            for (int k = 0; k < rotateIndex; k++)
+            {
+                char temp = ToRotate[0];
+                ToRotate[0] = ToRotate[ToRotate.Length - 1];
+                
+                for (int i = ToRotate.Length -1 ; i > 1; i--)
+                {
+                    ToRotate[i] = ToRotate[i - 1];
+                }
+
+                ToRotate[1] = temp;
+            }
+
+            if (needRevers)
+            {
+                ToRotate.Reverse();
+            }
         }
     }
 }
